3cf8c62e9690b09977a2bc1e9678a66a0e672f35
Daniel Levi-Minzi <dleviminzi@gmail.com>
2025-02-12 15:13:29 -0500

    skip in flight and link remap

--- libcontainer/criu_opts_linux.go
+++ libcontainer/criu_opts_linux.go
@@ -17,6 +17,7 @@ type CriuOpts struct {
 	LeaveRunning            bool               // leave container in running state after checkpoint
 	TcpEstablished          bool               // checkpoint/restore established TCP connections
 	TcpSkipInFlight         bool               // skip in-flight TCP connections
+	LinkRemap               bool               // allow one to link unlinked files back when possible
 	ExternalUnixConnections bool               // allow external unix connections
 	ShellJob                bool               // allow to dump and restore shell jobs
 	FileLocks               bool               // handle file locks, for safety
--- restore.go
+++ restore.go
@@ -42,6 +42,10 @@ using the runc checkpoint command.`,
 			Usage: "skip in-flight tcp connections",
 		},
 		cli.BoolFlag{
+			Name:  "link-remap",
+			Usage: "allow one to link unlinked files back when possible",
+		},
+		cli.BoolFlag{
 			Name:  "ext-unix-sk",
 			Usage: "allow external unix sockets",
 		},
